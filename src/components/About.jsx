b2e5822ee75fbf80786474c66b00a2332a491fbd
Rohan7072 <rohnaraikar007@gmail.com>
2024-09-02 22:03:07 +0530

    first commit

---
+++ src/components/About.jsx
@@ -0,0 +1,100 @@
+import React from "react";
+
+function About() {
+  return (
+    <div
+      name="About"
+      className="max-w-screen-2xl container mx-auto px-4 md:px-20 my-20 px-2 text-gray-400 "
+    >
+      <div>
+        <h1 className="text-3xl font-bold mb-5 text-pink-700 font-bold">About</h1>
+        <p>
+          Hello, I'm Rohan, a passionate Web developer with a keen eye for MERN
+          Stack . With a background in IT, I strive to create impactful and
+          visually stunning Software solutions that leave a lasting impression.
+        </p>
+        <br />
+        <h1 className="text-gray-200 font-semibold text-xl">
+          Education & Training
+        </h1><br/>
+        <div>
+    <div>
+        <h2 className="clg1">Dr. D.Y. Patil Institute of Computer Application, Akurdi, Pune</h2>
+        <p><strong>Master of Computer Application</strong></p>
+        <p>Dec. 2021 – Sep 2023</p>
+        <p>Percentage: 74.71%</p>
+    </div>
+    <br/>
+    <div>
+        <h2  className="clg2">Annasaheb Magar Mahavidyalaya, Hadapsar, Pune</h2>
+        <p><strong>Bachelor in Computer Application</strong></p>
+        <p>July 2018 – Sep 2021</p>
+        <p>Percentage: 74.45%</p>
+    </div>
+</div>
+
+        <br />
+        <br />
+        <h1 className="text-gray-200 font-semibold text-xl">
+          Technical Skills & Expertise
+        </h1><br/>
+        <span>
+         <h3>Languages: C++,C,Java, Python, JavaScript, PHP. </h3>
+         <h3>Libraries/Frameworks: Django,React,Tailwind CSS, Bootstrap, Numpy,Pandas, matplotlib, Seaborn</h3>
+         <h3>Database: MySQL, SQLite, Firebase.</h3>
+         <h3>Developer Tools: Git, Android Studio, Visual Studio, VS Code.</h3>
+         <h3>IT Construct: DSA, Operating System, DBMS, OOPS </h3>
+         <h3>Methodologies: Waterfall, Agile </h3>
+         
+        </span>
+        <br />
+        <br />
+        <h1 className="text-gray-200 font-semibold text-xl">
+          Professional Experience
+        </h1><br/>
+        <span>
+         <h3>Huda’s Technologies&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;  Jan. 2023 – July 2023 </h3>
+         <h3>Position: Software Developer Intern</h3>
+         <h3>&nbsp;&nbsp; • Developed and maintained responsive web pages using HTML, CSS, and JavaScript, ensuring cross-browser 
+         compatibility and mobile responsiveness. </h3>
+         <h3>&nbsp;&nbsp;• Built dynamic features and functionalities in web applications using PHP and JavaScript, enhancing user interaction 
+         and experience. </h3>
+         <h3>&nbsp;&nbsp;• Utilized Bootstrap to create visually appealing and responsive UI components, streamlining the web development 
+         process. </h3>
+         <h3>&nbsp;&nbsp;• Collaborated with a team of developers and designers, following agile methodologies and participating in code 
+         reviews. </h3>
+         <h3>&nbsp;&nbsp;• Assisted in debugging and optimizing code for improved performance and scalability.</h3>
+        </span>
+        <br />
+        <br />
+        <h1 className="text-gray-200 font-semibold text-xl">
+          Certificates
+        </h1><br/>
+        <span>
+        <h3>-DSA in C++ by Codehelp </h3> 
+        <h3>-LLD (Low Level Design) by Code help</h3>                                                                                                                            
+        <h3>-Basic of Microsoft Dynamics 365by Great Learning</h3> 
+        <h3>-Aws Beginner Course by Edureka ( Credential ID L2YFAMKC1 )</h3> 
+        <h3>-JavaScript from Lets Upgrade</h3> 
+        <h3>-React from Lets Upgrade</h3> 
+        <h3>-Introduction to generative A.I. From AWS Linear Regression (Artificial Intelligence) from up Grad</h3>
+        <h3>-Java from Lets Upgrade</h3>   
+        <h3>-Python from IIT Bombay</h3>                                                                                        
+ 
+        </span>
+        <br />
+        <br />
+        <h1 className="text-gray-200 font-semibold text-xl">
+          Languages
+        </h1><br/>
+        <p>
+          <h3>• English</h3>
+          <h3>• Hindi</h3>
+          <h3>• Marathi</h3>
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default About;
---
+++ src/components/Home.jsx
@@ -0,0 +1,99 @@
+import React from "react";
+
+import pic from "../../public/photo.png";
+
+import { FaSquareFacebook } from "react-icons/fa6";
+import { FaLinkedin } from "react-icons/fa";
+import { IoLogoYoutube } from "react-icons/io";
+import { FaTelegram } from "react-icons/fa6";
+
+import { SiMongodb } from "react-icons/si";
+import { SiExpress } from "react-icons/si";
+import { FaReact } from "react-icons/fa";
+import { FaNodeJs } from "react-icons/fa6";
+
+import { ReactTyped } from "react-typed";
+
+function Home() {
+  return (
+    <>
+      <div
+        name="Home"
+        className="max-w-screen-2xl container mx-auto px-4 md:px-20 my-28"
+      >
+        <div className="flex flex-col md:flex-row">
+          <div className="md:w-1/2 mt-12 md:mt-24 space-y-2 order-2 md:order-1">
+            <span className="text-xl">Welcome In My Feed</span>
+            <div className="flex space-x-1 text-2xl md:text-4xl">
+              <h1 className="heading">Hello, I'm a</h1>
+              {/* <span >Developer</span> */}
+              <ReactTyped
+                className="text-pink-700 font-bold"
+                strings={["Developer", "Programmer", "Coder"]}
+                typeSpeed={40}
+                backSpeed={50}
+                loop={true}
+              />
+            </div>
+            <br />
+            <p className="text-sm md:text-md text-justify">
+            
+My name is Rohan Raikar and I am born and brought up in Pune, where I completed my schooling. I graduated with a degree in Computer Applications and I am recently completed my master's in the same field. As of now, I am a computer programmer with knowledge of various programming languages like C, C++, Java, Python, PHP, and JavaScript,React,Nodejs. 
+And I have also completed internship from Huda's technologies on web development. 
+Apart from programming, I also enjoy riding a bike, playing Cricket.
+            </p>
+            <br />
+            {/* social media icons */}
+            <div className="flex flex-col items-center md:flex-row justify-between space-y-6 md:space-y-0 text-pink-700 font-bold">
+              <div className="  space-y-2">
+                <h1 className="font-bold text-center ">Available on</h1>
+                <ul className="flex space-x-5">
+                  <li>
+                    <a href="https://www.facebook.com/" target="_blank">
+                      <FaSquareFacebook className="text-2xl cursor-pointer text-blue-700 font-bold" />
+                    </a>
+                  </li>
+                  <li>
+                    <a href="https://www.linkedin.com/" target="_blank">
+                      <FaLinkedin className="text-2xl cursor-pointer text-blue-700 font-bold" />
+                    </a>
+                  </li>
+                  <li>
+                    <a href="https://www.youtube.com/" target="_blank">
+                      <IoLogoYoutube className="text-2xl cursor-pointer text-red-700 font-bold" />
+                    </a>
+                  </li>
+                  <li>
+                    <a href="https://t.me/" target="_blank">
+                      <FaTelegram className="text-2xl cursor-pointer text-blue-700 font-bold" />
+                    </a>
+                  </li>
+                </ul>
+              </div>
+              <div className=" space-y-2">
+                <h1 className="font-bold text-center">Currently working on</h1>
+                <div className="flex space-x-5">
+                  <SiMongodb className="text-2xl md:text-3xl hover:scale-110 duration-200 rounded-full border-[2px] cursor-pointer text-green-700 font-bold " />
+                  <SiExpress className="text-2xl md:text-3xl hover:scale-110 duration-200 rounded-full border-[2px] cursor-pointer text-green-700 font-bold" />
+                  <FaReact className="text-2xl md:text-3xl hover:scale-110 duration-200 rounded-full border-[2px] cursor-pointer text-blue-700 font-bold" />
+                  <FaNodeJs className="text-2xl md:text-3xl hover:scale-110 duration-200 rounded-full border-[2px] cursor-pointer text-yellow-400 font-bold" />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="md:w-1/2 md:ml-48 md:mt-20 mt-8 order-1">
+            <img
+              src={pic}
+              className="rounded-full md:w-[450px] md:h-[450px]"
+              alt=""
+            />
+          </div>
+        </div>
+      </div>
+
+      <hr />
+    </>
+  );
+}
+
+export default Home;
